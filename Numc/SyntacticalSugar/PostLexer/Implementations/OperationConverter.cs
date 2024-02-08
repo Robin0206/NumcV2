4b2473a05092ad215cf0f34a969baf5cab687d48
Robin <Robin.Kroker@gmx.de>
2024-02-08 12:13:21 +0100

    "OperationConverterSkeleton"

---
+++ Numc/SyntacticalSugar/PostLexer/Implementations/OperationConverter.cs
@@ -0,0 +1,17 @@
+﻿using Numc.lexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numc.SyntacticalSugar.PostLexer.Implementations
+{
+    class OperationConverter : PostLSSLayer
+    {
+        public List<Token> removeSugar(List<Token> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
